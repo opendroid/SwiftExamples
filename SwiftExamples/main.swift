6347232040ed3064bce683093fdcf3241511a0d7
opendriod <ajaythakur@hotmail.com>
2016-04-20 14:18:34 -0700

    Initial Commit

---
+++ SwiftExamples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExamples
+//
+//  Created by Ajay Thakur on 4/20/16.
+//  Copyright © 2016 Ajay Thaur. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
